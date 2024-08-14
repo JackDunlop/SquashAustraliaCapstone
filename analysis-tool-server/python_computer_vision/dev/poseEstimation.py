3655b9ea1f193cdf84092db20c78c5c0985f2677
Jack Dunlop <jackdunlops@icloud.com>
2024-08-14 21:43:48 +1000

    New routes and refactor of pose

--- analysis-tool-server/python_computer_vision/dev/poseEstimation.py
+++ analysis-tool-server/python_computer_vision/dev/poseEstimation.py
@@ -1,9 +1,4 @@
 
-''' 
-    **********************************************************************************************************************************************************************************
-    *************************************************************************** IMPORTS **********************************************************************************************
-    **********************************************************************************************************************************************************************************
- '''
 
 import sys
 import os
@@ -13,89 +8,6 @@ from enum import Enum
 import time
 import numpy as np
 import json
-
-''' 
-    **********************************************************************************************************************************************************************************
-    *************************************************************************** CUSTOM ERRORS ***************************************************************************************
-    ********************************************************************************************************************************************************************************** 
-'''
-
-class ApplicationError(Exception):
-    """Parent class for custom errors."""
-    pass
-
-class PathDoesNotExistError(ApplicationError):
-    """Exception raised when the specified file does not exist."""
-    def __init__(self, file, message="Files path does not exist, DO NOT PASS IN RELATIVE PATH, MUST PASS IN FULL PATH."):
-        self.file = file
-        self.message = message
-        super().__init__(f"{message}: {file}")
-
-class OutputFolderDoesNotExistError(ApplicationError):
-    """Exception raised when the output folder does not exist."""
-    def __init__(self, file, message="Output folder does not exist, you need a folder to store the output videos names outputVideos."):
-        self.file = file
-        self.message = message
-        super().__init__(f"{message}: {file}")
-
-class UniqueIdentifierIsntUnique(ApplicationError):
-    """Exception raised when the passed unique identifier already existis."""
-    def __init__(self, uniqueIdentifier, message="Unique identifier already existis. "):
-        self.file = uniqueIdentifier
-        self.message = message
-        super().__init__(f"{message}: {uniqueIdentifier}")
-
-class InvalidRequiredArgumentsError(ApplicationError):
-    """Exception raised when three arguments have not been passed in."""
-    def __init__(self, arguments, message="Must have three passed in arguments."):
-        self.arguments = arguments[1:]  # skip the script name
-        self.message = message
-        super().__init__(f"{message}: Arguments Passed in {self.arguments}")
-
-
-
-''' 
-    **********************************************************************************************************************************************************************************
-    *************************************************************************** HANDLING ARUGMENTS  **********************************************************************************
-    ********************************************************************************************************************************************************************************** 
-'''
-
-class Arguments:
-    def __init__(self, videoPath, uniqueIdentifier,videoDataOutPutPath):
-        self.videoPath = videoPath
-        self.uniqueIdentifier = uniqueIdentifier
-        self.videoDataOutPutPath = videoDataOutPutPath
-
-    def checkArgumentLength(argv):
-        if len(argv) == 4:
-            args = Arguments(argv[1], argv[2], argv[3])
-            return args
-        else:
-            raise InvalidRequiredArgumentsError(argv)
-    
-    def checkPathExists(self):
-        if not os.path.isfile(self.videoPath):
-            raise PathDoesNotExistError(self.videoPath)
-    
-    def checkOutputFolderExists(self):
-        currentScriptDirectory = os.path.dirname(os.path.abspath(__file__))
-        outputFolderPath = os.path.join(currentScriptDirectory, "outputVideos")
-        if not os.path.exists(outputFolderPath):
-            raise OutputFolderDoesNotExistError(outputFolderPath)
-        return outputFolderPath
-
-    def checkFileIdentifierIsUnique(self, outputFolderPath):
-        fileNames = [f for f in os.listdir(outputFolderPath) if os.path.isfile(os.path.join(outputFolderPath, f))]
-        parsedFilenames = [filename.split('_')[0] for filename in fileNames]
-        if self.uniqueIdentifier in parsedFilenames:
-            raise UniqueIdentifierIsntUnique(self.uniqueIdentifier)
-    
-''' 
-    **********************************************************************************************************************************************************************************
-    *************************************************************************** VIDEO FILE  ******************************************************************************************
-    ********************************************************************************************************************************************************************************** 
-'''
-    
 class GetKeypoint(Enum):
     NOSE:           int = 0
     LEFT_EYE:       int = 1
@@ -115,47 +27,17 @@ class GetKeypoint(Enum):
     LEFT_ANKLE:     int = 15
     RIGHT_ANKLE:    int = 16
 
-
-def calculateAngle(p1, p2, p3):
-    p1 = np.array(p1)
-    p2 = np.array(p2)
-    p3 = np.array(p3)
-    
-    if np.linalg.norm(p1) == 0 or np.linalg.norm(p2) == 0 or np.linalg.norm(p3) == 0:
-        return None
-
-    v1 = p1 - p2
-    v2 = p3 - p2
-    
-    norm_v1 = np.linalg.norm(v1)
-    norm_v2 = np.linalg.norm(v2)
-    
-    if norm_v1 == 0 or norm_v2 == 0:
-        return None
-
-    cos_theta = np.dot(v1, v2) / (norm_v1 * norm_v2)
-    cos_theta = np.clip(cos_theta, -1.0, 1.0)
-    
-    angle_rad = np.arccos(cos_theta)
-    angle_deg = np.degrees(angle_rad)
-    
-    return angle_deg
-
 def poseEstimation(videoPath):
     model_path = 'models/yolov8m-pose.pt'  
     model = YOLO(model_path) 
     confThresh = 0.80
     modelClass = [0]
     cap = initialiseVideoCapture(videoPath)
-    
     if not cap:
         return
-
-    
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
-
     match_id = getMatchIDFromVideo(videoPath)
     script_dir = os.path.dirname(os.path.abspath(__file__))
     output_dir = os.path.join(script_dir, '..', '..', 'poseOutputVideo')
@@ -163,33 +45,21 @@ def poseEstimation(videoPath):
     filesave = os.path.join(output_dir, f'{match_id}.mp4')
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
     out = cv2.VideoWriter(filesave, fourcc, fps, (frame_width, frame_height))
-
     frameData = []
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-        
-        frame_resized = cv2.resize(frame, (640, 640))
         frameTimestamp = getFrameTimestamp(cap)
         detection = getDetection(model, frame, confThresh, modelClass)
-        
         if detection is None:
             continue
-
         processDetection(detection, frameTimestamp, frameData, frame)
-
-        
         out.write(frame)
-        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-
-    finalizeVideoProcessing(cap, frameData, videoPath)
+    finaliseVideoProcessing(cap, frameData, videoPath)
     out.release()
-
-
 
 def initialiseVideoCapture(videoPath):
     cap = cv2.VideoCapture(videoPath)
@@ -221,11 +91,7 @@ def processDetection(detection, frameTimestamp, frameData, frame):
     track_ids = detection[0].boxes.id.cpu().numpy()  
     for track_id, kptArray in zip(track_ids, keypoints):
         keypointData = extractKeypointData(kptArray, frame, track_id)
-        calculateAngleJoints(keypointData,'LEFT_SHOULDER','LEFT_ELBOW','LEFT_WRIST','LEFT_ARM_ANGLE',frame)
-        calculateAngleJoints(keypointData,'RIGHT_SHOULDER','RIGHT_ELBOW','RIGHT_WRIST','RIGHT_ARM_ANGLE',frame)
-        calculateAngleJoints(keypointData,'LEFT_HIP','LEFT_KNEE','LEFT_ANKLE','LEFT_LEG_ANGLE',frame)
-        calculateAngleJoints(keypointData,'RIGHT_HIP','RIGHT_KNEE','RIGHT_ANKLE','RIGHT_LEG_ANGLE',frame)
-        
+ 
         frameData.append({
             'track_id': int(track_id),
             'timestamp': frameTimestamp,
@@ -240,31 +106,14 @@ def extractKeypointData(kptArray, frame, track_id):
         keypointName = GetKeypoint(pointIndex).name
         keypointData[keypointName] = [x, y]
         cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
-        # if keypointName == "LEFT_SHOULDER":
-        #     cv2.putText(frame, f"{track_id}", (x, y - 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-        
     return keypointData
-
-def calculateAngleJoints(keypointData, p1String, p2String, p3string, specifcJoint,frame):
-    p1 = keypointData[p1String]
-    p2 = keypointData[p2String]
-    p3 = keypointData[p3string]
-    
-    if any(np.array_equal(point, [0, 0]) for point in [p1, p2, p3]):
-        keypointData[specifcJoint] = "Can't Calculate Angle" 
-    else:
-        angle = calculateAngle(p1, p2, p3)
-        keypointData[specifcJoint] = angle 
-        # if specifcJoint == 'RIGHT_ARM_ANGLE':
-        #     cv2.putText(frame, f"{specifcJoint}: {angle:.2f}", (p2[0], p2[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (124,252,0), 2)
-
 
 def getMatchIDFromVideo(video_path):
     baseName = os.path.basename(video_path)
     match_id = os.path.splitext(baseName)[0]
     return match_id            
 
-def finalizeVideoProcessing(cap, frameData, videoPath):
+def finaliseVideoProcessing(cap, frameData, videoPath):
     cap.release()
     cv2.destroyAllWindows()
     match_id = getMatchIDFromVideo(videoPath)
@@ -278,9 +127,6 @@ def finalizeVideoProcessing(cap, frameData, videoPath):
 def main():
     videoPath = sys.argv[1]
     poseEstimation(videoPath)
-   
-   
-
+    
 if __name__ == "__main__":
     main()
-

b36d6e8e22a130d4b8d8dd81a2c5fd7e4e10b4f1
weenzz <114854970+weenzz@users.noreply.github.com>
2024-08-21 12:36:34 +1000

    Clean up repository and update .gitignore

--- analysis-tool-server/PIPDOWNLOADLIST.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pip install numpy
-pip install opencv-python
-pip install scikit-learn
-pip install ultralytics
-pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --upgrade
-pip install lapx
-:: double click or run in terminal ./PIPDOWNLOADLIST.bat THIS MIGHT TAKE SOMETIME!

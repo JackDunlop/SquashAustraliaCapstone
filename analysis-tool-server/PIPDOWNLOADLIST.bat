69b835fcb6d87bd2b58fa3f15d08660ca0e683a4
kimwoodfield <kimwoodfield@live.com>
2024-09-02 21:30:19 +1000

    specify dep versions

--- analysis-tool-server/PIPDOWNLOADLIST.bat
+++ analysis-tool-server/PIPDOWNLOADLIST.bat
@@ -1,9 +1,11 @@
 @echo off
-pip install numpy
-pip install opencv-python
-pip install scikit-learn
-pip install ultralytics
-pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --upgrade
-pip install lapx
-pip install msgpack
+pip install numpy==1.26.4
+pip install opencv-python==4.10.0.84
+pip install scikit-learn==
+pip install ultralytics==8.2.76
+pip install torch==2.0.0 
+pip install torchvision==0.15.0
+pip install torchaudio==2.0.0
+pip install lapx==0.5.9.post1
+pip install msgpack==1.0.8
 :: double click or run in terminal ./PIPDOWNLOADLIST.bat THIS MIGHT TAKE SOMETIME!
